b50c90d47a5de836a9eab02ccf507fa7fbb38e7d
Rolan Zaripov <buldozerr21@gmail.com>
2024-10-16 17:35:31 +0300

    hw1 fix

--- Point/parsepoint/parsepoint.go
+++ Point/parsepoint/parsepoint.go
@@ -3,24 +3,26 @@ package parsepoint
 import (
 	p "Point/point"
 	"errors"
+	"fmt"
 	"strconv"
 	"strings"
 )
 
 func Parse(input *string) (p.Point, error) {
-	//check if nil
+	if input == nil {
+		return p.Point{}, errors.New("bad input. nil pointer in input")
+	}
 	pSlice := strings.Split(*input, ",")
 	if len(pSlice) < 2 {
 		return p.Point{}, errors.New("bad input. expected - x,y got - " + *input)
 	}
-	//err should be unique
 	x, err := strconv.ParseFloat(pSlice[0], 32)
 	if err != nil {
-		return p.Point{}, err
+		return p.Point{}, fmt.Errorf("error parsing x - %v", err)
 	}
 	y, err := strconv.ParseFloat(pSlice[1], 32)
 	if err != nil {
-		return p.Point{}, err
+		return p.Point{}, fmt.Errorf("error parsing y - %v", err)
 	}
 	point := p.Point{X: float32(x), Y: float32(y)}
 	return point, nil
