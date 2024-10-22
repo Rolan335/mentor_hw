5664e86588b7790849ed4b16d106be971a502c60
Rolan Zaripov <buldozerr21@gmail.com>
2024-10-22 15:27:05 +0300

    zero validation, Raw string support

--- StringUnpack/main.go
+++ StringUnpack/main.go
@@ -8,7 +8,7 @@ import (
 func main(){
 	//TODO: zero validation and another tasks.
 	//`qwe\\\\45`
-	input := `qwe45`
+	input := `f0w0ef\3n\\0\\`
 	fmt.Println(unpack.Unpack(input,true))
 	// fmt.Println(unpack.Unpack(input,true))
 }
--- StringUnpack/unpack/unpack.go
+++ StringUnpack/unpack/unpack.go
@@ -7,27 +7,6 @@ import (
 	"unicode"
 )
 
-// var buf strings.Builder
-// runes := []rune(str)
-// for i, v := range runes {
-// 	if toInt, err := strconv.Atoi(string(v)); err == nil {
-// 		if i != len(runes)-1 {
-// 			if _, err := strconv.Atoi(string(runes[i+1])); err == nil {
-// 				return ""
-// 			}
-// 		}
-// 		buf.WriteString(strings.Repeat(string(runes[i-1]), toInt-1))
-// 	} else {
-// 		buf.WriteRune(v)
-// 	}
-// }
-// return buf.String()
-
-// type FormattedString struct{
-// 	str string
-// 	isRaw bool
-// }
-
 func Unpack(str string, isRaw bool) any {
 	if str == "" {
 		return ""
@@ -36,7 +15,6 @@ func Unpack(str string, isRaw bool) any {
 		str = strings.ReplaceAll(str, `\\`, `+`)
 		str = strings.ReplaceAll(str, `\`, "_")
 	}
-	fmt.Println(str)
 	runes := []rune(str)
 	if unicode.IsDigit(runes[0]) {
 		return "wrong string"
@@ -44,16 +22,27 @@ func Unpack(str string, isRaw bool) any {
 	var buf strings.Builder
 	for i, v := range runes {
 		if isRaw {
-			if i != 0 {
+			//runes[i-1] panics if checks first letter
+			if i != 0 && i != len(runes)-1 {
+				if unicode.IsDigit(v) && runes[i-1] != '_' && unicode.IsDigit(runes[i+1]) {
+					return "wrong string"
+				}
 				if unicode.IsLetter(v) && runes[i-1] == '_' {
 					return "wrong string"
 				}
 			}
-			if unicode.IsDigit(v) && runes[i-1] != '_' {
+			if unicode.IsDigit(v) && runes[i-1] != '_' && int(v-49) >= 0 {
 				buf.WriteString(strings.Repeat(string(runes[i-1]), int(v-49)))
 				continue
 			}
-			//runes[i-1] panics if checks first letter
+			if i != len(runes)-1{
+				if runes[i+1] == '0' {
+					continue
+				}
+			}
+			if runes[i] == '0' && runes[i-1] != '_'{
+				continue
+			}
 			buf.WriteString(string(v))
 			continue
 		}
@@ -61,11 +50,23 @@ func Unpack(str string, isRaw bool) any {
 			if unicode.IsDigit(runes[i-1]) {
 				return "wrong string"
 			}
-			buf.WriteString(strings.Repeat(string(runes[i-1]), int(v-49)))
+			fmt.Println(string(v))
+			if int(v-49) > 0 {
+				buf.WriteString(strings.Repeat(string(runes[i-1]), int(v-49)))
+				continue
+			}
+			continue
+		}
+		// Is there better way to check if out of range
+		if i != len(runes)-1 {
+			if runes[i+1] == '0' {
+				continue
+			}
+		}
+		if runes[i] == '0' {
 			continue
 		}
 		buf.WriteString(string(v))
-		fmt.Println(buf.String())
 	}
 	str = strings.ReplaceAll(buf.String(), "_", "")
 	str = strings.ReplaceAll(str, "+", `\`)
@@ -73,68 +74,4 @@ func Unpack(str string, isRaw bool) any {
 		return str
 	}
 	return strconv.Quote(str)
-}
-
-func unpackRaw(runes []rune) any {
-	strSlice := []string{}
-	for i, v := range runes {
-		fmt.Print(string(v))
-		if string(v) == `\` && (string(runes[i+1]) == `\` || unicode.IsDigit(runes[i+1])) {
-			fmt.Print(true)
-			fmt.Println()
-			strSlice = append(strSlice, string(v), string(runes[i+1]))
-		} else {
-			strSlice = append(strSlice, string(v))
-			fmt.Println()
-		}
-	}
-	fmt.Println(strSlice)
-	return strSlice
-}
-
-func ProcessString(s string) (string, error) {
-	result := ""
-	skipNext := false
-
-	for i, r := range s {
-		if skipNext {
-			// Если предыдущий символ был слэш, добавляем текущий символ (если он не вне диапазона)
-			if unicode.IsDigit(r) || r == '\\' {
-				result += string(r)
-				skipNext = false // Сбрасываем флаг
-			} else {
-				return "", fmt.Errorf("некорректная строка на позиции %d", i)
-			}
-			continue
-		}
-
-		if r == '\\' {
-			// Устанавливаем флаг для пропуска следующего символа
-			skipNext = true
-			continue
-		}
-
-		result += string(r)
-	}
-
-	// Проверяем, остался ли флаг с экранированием
-	if skipNext {
-		return "", fmt.Errorf("некорректная строка: конечный слэш найден")
-	}
-
-	// Заменяем цифры на соответствующее количество цифр
-	finalResult := ""
-	for _, r := range result {
-		if unicode.IsDigit(r) {
-			count := int(r - '0')    // Преобразуем символ цифры в число
-			finalResult += string(r) // Добавляем саму цифру
-			for j := 1; j < count; j++ {
-				finalResult += string(r) // Добавляем цифру столько же раз, сколько указано
-			}
-		} else {
-			finalResult += string(r) // Добавляем обычные символы
-		}
-	}
-
-	return finalResult, nil
 }
