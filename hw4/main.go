d1a97c5491cd9788025a97e8ca197db18e8a5907
Rolan Zaripov <buldozerr21@gmail.com>
2024-11-27 15:39:03 +0300

    hw4 pi 2.2

--- hw4/main.go
+++ hw4/main.go
@@ -10,5 +10,7 @@ func main() {
 	input := flag.Int("goroutines", 1, "enter num of goroutines")
 	flag.Parse()
 
-	fmt.Println(pi.Pi(*input))
+	// fmt.Println(pi.Pi(*input))
+
+    fmt.Println(pi.Pi2(*input))
 }
--- hw4/pi/pi.go
+++ hw4/pi/pi.go
@@ -5,95 +5,46 @@ import (
 	"math"
 	"os"
 	"os/signal"
-	"runtime"
 	"sync"
 	"syscall"
 )
 
-//runtime.NumCPU() = 12
-//1 goroutine 30 sec calc
-//251366897 iter
-//3.141592657567563
-//6 goroutines 30 sec
-//140674779 iter
-//3.141592652412406
-//12 goroutines 30 sec
-//128643665 iter
-//3.141592652939999
-
-var logicalProcessors int = runtime.NumCPU()
-
-//Можно было изначально задать максимальное значение итераций как количество членов ряда которые нужно вычислить
-
-// Сделано на костыле. Проблема - не синхронизированно количество итераций при кол-ве горутин >1
-func calcRow(i int, input int, sig <-chan os.Signal, sumCh chan<- float64, wg *sync.WaitGroup, maxIter *int) {
+func calcRow(wg *sync.WaitGroup, i int, sumCh chan float64) {
 	defer wg.Done()
-	var sum float64
-	ticker := 0
-	waitedIters := 0
-	//считаем свои числа ряда в default до того, как получим сигнал syscall
-	for {
-		select {
-		case <-sig:
-			//когда получен сигнала sigint в другой горутине заканчивается инкрементирование maxIter. Горутина догоняет счётчик для равного кол-ва итераций
-			for ; ticker <= *maxIter; ticker++ {
-				sum += math.Pow(float64(-1), float64(i)) / float64(2*i+1)
-				i += input
-			}
-			fmt.Println("iters done: ", ticker)
-			fmt.Println("waited iters: ", waitedIters)
-			sumCh <- sum
-			return
-		default:
-			//если не выступаем за счётчик, считаем ряд, иначе ждём инкрементации maxIter
-			if ticker <= *maxIter {
-				sum += math.Pow(float64(-1), float64(i)) / float64(2*i+1)
-				i += input
-				ticker++
-			} else {
-				waitedIters++
-			}
-		}
-	}
+	sum := math.Pow(float64(-1), float64(i)) / float64(2*i+1)
+	sumCh <- sum
 }
 
 func Pi(input int) float64 {
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
-	sumCh := make(chan float64, input)
-	maxIter := 1_000_000
 	var wg sync.WaitGroup
-	for i := 0; i < input; i++ {
-		wg.Add(1)
-		go calcRow(i, input, sig, sumCh, &wg, &maxIter)
-	}
-	//костыль для контроля итераций. Чтобы при сигнале sigint у горутин было одинаковое количество итераций.
-	go func(maxIter *int) {
-		for ticker := 0; ; ticker++ {
-			select {
-			case <-sig:
-				//почему при переносе закрытия sig сюда, программа работаем нестабильно
-				return
-			default:
-				// magic formula, каждую 12 итерацию увеличиваем счётчик на 1. Баланс между тем чтобы не тормозить горутины и чтобы счётчик
-				// не убегал далеко вперёд ????
-				if ticker%logicalProcessors == 0 {
-					*maxIter += 1
-				}
-			}
-		}
-	}(&maxIter)
-	<-sig
-	close(sig)
-	wg.Wait()
-	close(sumCh)
-
 	var sum float64
-	for i := range sumCh {
-		sum += i
+	var sumCh chan float64
+	rowCounter := 0
+	ticker := 0
+	for {
+		select {
+		case <-sig:
+			close(sig)
+			wg.Wait()
+			fmt.Println(ticker * input)
+			return sum * 4
+		default:
+			ticker++
+			sumCh = make(chan float64, input)
+			for i := 0; i < input; i++ {
+				wg.Add(1)
+				go calcRow(&wg, rowCounter+i, sumCh)
+			}
+			wg.Wait()
+			close(sumCh)
+			for v := range sumCh {
+				sum += v
+			}
+			rowCounter += input
+		}
 	}
-	sum *= 4
-	return sum
 }
 
 //Количество итераций не синхронизировано
